e9026ca77d3280053110a15c47d837e27eb6cf8a
vivek-6 <vivek661999@gmail.com>
2022-09-16 16:46:08 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+#### How to run project ?
+``` 
+npm install
+npm start
+```
---
+++ src/pages/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+         This website is created by vivek singh!
+      </p>
+    </section>
+  );
+}
